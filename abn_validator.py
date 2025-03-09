9ac785dfc9b7c82f0fc2eb83446f1aed97662d3b
helenlex <154233295+helenlex@users.noreply.github.com>
2025-03-09 22:18:01 +1100

    testing to see if my 2nd commit works

--- abn_validator.py
+++ abn_validator.py
@@ -1,3 +1,5 @@
-abn = input('Please input an ABN without spaces:')
-
+#abn = input('Please input an ABN without spaces:')
+abn = str(51824753556)
 #Subtract 1 from the first (left-most) digit of the ABN to give a new 11 digit number
+newabn =(str(int(abn[0]) - 1)) + abn[1:]
+print(newabn)
